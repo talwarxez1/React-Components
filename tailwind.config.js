d3cfd9bd17b9f43628f193c1463becf4972ddc9b
Vibhor Talwar <93029550+talwarxez1@users.noreply.github.com>
2024-07-29 07:50:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      keyframes: {
+        animation: {
+          '0%, 100%': { top: '0' },
+          '20%': { top: '0' },
+          '25%': { top: '-50px' },
+          '45%': { top: '-50px' },
+          '50%': { top: '-100px' },
+        },
+      },
+      animation: {
+        'custom-animation': 'animation 10s ease infinite',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
